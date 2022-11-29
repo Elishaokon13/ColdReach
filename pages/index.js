2644f6b2adcd859b134c9ac8f87c3168be16dcff
eliscodavid@gmail.com <eliscodavid@gmail.com>
2022-11-29 09:59:05 +0100

    index.js

--- pages/index.js
+++ pages/index.js
@@ -1,80 +1,85 @@
-import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 import { useState } from 'react';
 
-
 const Home = () => {
+
   const [userInput, setUserInput] = useState('');
+
   const [apiOutput, setApiOutput] = useState('')
-const [isGenerating, setIsGenerating] = useState(false)
+  const [isGenerating, setIsGenerating] = useState(false)
 
-const callGenerateEndpoint = async () => {
-  setIsGenerating(true);
-  
-  console.log("Calling OpenAI...")
-  const response = await fetch('/api/generate', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ userInput }),
-  });
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
 
-  const data = await response.json();
-  const { output } = data;
-  console.log("OpenAI replied...", output.text)
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
 
-  setApiOutput(`${output.text}`);
-  setIsGenerating(false);
-}
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
   const onUserChangedText = (event) => {
-  console.log(event.target.value);
-  setUserInput(event.target.value);
-};
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>Publicación de blog Escritor de IA</h1>
+            <h1>magic blog post generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>Hi👋 there! I'm Kasper. I could generate blog posts in seconds. Give me your blog title. </h2>
+            <h2>
+              input the title to your blog post below, we'll generate the
+              rest.
+            </h2>
           </div>
         </div>
-        {/* Add this code here*/}
         <div className="prompt-container">
-  <textarea
-    placeholder="start typing here"
-    className="prompt-box"
-    value={userInput}
-    onChange={onUserChangedText}
-  />
- <div className="prompt-buttons">
-  <a
-    className={isGenerating ? 'generate-button loading' : 'generate-button'}
-    onClick={callGenerateEndpoint}
-  >
-    <div className="generate">
-    {isGenerating ? <span class="loader"></span> : <p>Generate</p>}
-    </div>
-  </a>
-</div>
-  {/* New code I added here */}
-  {apiOutput && (
-  <div className="output">
-    <div className="output-header-container">
-      <div className="output-header">
-        <h3>Output</h3>
-      </div>
-    </div>
-    <div className="output-content">
-      <p>{apiOutput}</p>
-    </div>
-  </div>
-)}
-</div>
+          <textarea
+            placeholder="Top 3 Animes of All Time"
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a
+              className={
+                isGenerating ? 'generate-button loading' : 'generate-button'
+              }
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+              {isGenerating ? <span class="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
+          {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            {/* New code I added here */}
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
+          </div>
+        )}
+        </div>
       </div>
       <div className="badge-container grow">
         <a
@@ -91,3 +96,5 @@ const callGenerateEndpoint = async () => {
     </div>
   );
 };
+
+export default Home;
