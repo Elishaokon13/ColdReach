51a7d063448c1f5217af59b090975424c80db798
oxbryte <silascyrax@gmail.com>
2024-08-15 01:04:22 +0100

    chore: Update Node.js version requirement to >=18

--- pages/_app.js
+++ pages/_app.js
@@ -2,29 +2,29 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "./styles.css";
 import theme from "../src/theme/theme";
 // Supports weights 100-900
-import '@fontsource-variable/inter';
+import "@fontsource-variable/inter";
 
-import { createWeb3Modal, defaultConfig } from '@web3modal/ethers/react'
+import { createWeb3Modal, defaultConfig } from "@web3modal/ethers/react";
 
 // 1. Get projectId
-const projectId = 'YOUR_PROJECT_ID'
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 
 // 2. Set chains
 const mainnet = {
   chainId: 1,
-  name: 'Ethereum',
-  currency: 'ETH',
-  explorerUrl: 'https://etherscan.io',
-  rpcUrl: 'https://cloudflare-eth.com'
-}
+  name: "Ethereum",
+  currency: "ETH",
+  explorerUrl: "https://etherscan.io",
+  rpcUrl: "https://cloudflare-eth.com",
+};
 
 // 3. Create a metadata object
 const metadata = {
-  name: 'My Website',
-  description: 'My Website description',
-  url: 'https://mywebsite.com', // origin must match your domain & subdomain
-  icons: ['https://avatars.mywebsite.com/']
-}
+  name: "My Website",
+  description: "My Website description",
+  url: "https://mywebsite.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.mywebsite.com/"],
+};
 
 // 4. Create Ethers config
 const ethersConfig = defaultConfig({
@@ -37,17 +37,17 @@ const ethersConfig = defaultConfig({
   enableCoinbase: true, // true by default
   enableSwaps: true, // true by default
   enableOnramp: true, // true by default
-  rpcUrl: '...', // used for the Coinbase SDK
-  defaultChainId: 1 // used for the Coinbase SDK
-})
+  rpcUrl: "...", // used for the Coinbase SDK
+  defaultChainId: 1, // used for the Coinbase SDK
+});
 
 // 5. Create a AppKit instance
 createWeb3Modal({
   ethersConfig,
   chains: [mainnet],
   projectId,
-  enableAnalytics: true // Optional - defaults to your Cloud configuration
-})
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+});
 
 function App({ Component, pageProps }) {
   return (
