0948bea9a44b3f00b3db12f7d6fc6a8f999b0ad0
oxbryte <silascyrax@gmail.com>
2024-08-16 16:36:19 +0100

    feat: Add ProModal component for displaying premium plan details

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,11 @@
-import { Box, Button, Flex, useColorMode, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  useColorMode,
+  useDisclosure,
+  VStack,
+} from "@chakra-ui/react";
 import {
   useDisconnect,
   useWeb3Modal,
@@ -8,84 +15,98 @@ import React from "react";
 import { FaCrown } from "react-icons/fa6";
 import { truncateText } from "../lib/utils";
 import { BsMoonFill, BsSunFill } from "react-icons/bs";
+import ProModal from "./ProModal";
 
 export default function Navbar() {
   const { open } = useWeb3Modal();
   const { disconnect } = useDisconnect();
   const { address, isConnected } = useWeb3ModalAccount();
   const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const getColorModeStyle = (light, dark) =>
     colorMode === "light" ? light : dark;
 
   return (
-    <VStack
-      mx="auto"
-      my="24px"
-      maxW="760px"
-      align="flex-start"
-      borderWidth="1px"
-      borderColor={getColorModeStyle("blackAlpha.200", "whiteAlpha.200")}
-      px="16px"
-      py="8px"
-      rounded="xl"
-      // bg="whiteAlpha.200"
-      // backdropFilter="blur(3px)"
-      // position="fixed"
-      // top="10"
-      // left="50%"
-      // transform="translateX(-50%)"
-      // zIndex="1000"
-    >
-      <Flex align="center" justify="space-between" w="100%">
-        {/* <w3m-button balance="hide" /> */}
-        <Button
-          align="center"
-          gap="12px"
-          rounded="full"
-          variant="solid"
-          size="md"
-          color="white"
-          // minW="140px"
-          bgGradient="linear(to-br, blue.600, blackAlpha.800)"
-          _hover={{
-            bgGradient: "linear(to-tl, blue.600, blackAlpha.800)",
-          }}
-          fontSize={12}
-          onClick={() => {
-            isConnected ? open({ view: "Account" }) : open({ view: "Connect" });
-          }}
-        >
-          {isConnected ? <>{truncateText(address, 10)}</> : "Connect wallet"}
-        </Button>
-        <Flex align="center" gap="12px">
+    <>
+      <VStack
+        mx="auto"
+        my="24px"
+        maxW="760px"
+        align="flex-start"
+        borderWidth="1px"
+        borderColor={getColorModeStyle("blackAlpha.200", "whiteAlpha.200")}
+        px="16px"
+        py="8px"
+        rounded="xl"
+        // bg="whiteAlpha.200"
+        // backdropFilter="blur(3px)"
+        // position="fixed"
+        // top="10"
+        // left="50%"
+        // transform="translateX(-50%)"
+        // zIndex="1000"
+      >
+        <Flex align="center" justify="space-between" w="100%">
+          {/* <w3m-button balance="hide" /> */}
           <Button
             align="center"
             gap="12px"
             rounded="full"
-            variant="outline"
-            color={getColorModeStyle("blackAlpha.800", "whiteAlpha.800")}
-            borderColor={getColorModeStyle("blackAlpha.200", "whiteAlpha.200")}
+            variant="solid"
+            size="md"
+            color="white"
+            // minW="140px"
+            bgGradient="linear(to-br, blue.600, blackAlpha.800)"
             _hover={{
-              bg: getColorModeStyle("blackAlpha.50", "whiteAlpha.50"),
+              bgGradient: "linear(to-tl, blue.600, blackAlpha.800)",
             }}
             fontSize={12}
+            onClick={() => {
+              isConnected
+                ? open({ view: "Account" })
+                : open({ view: "Connect" });
+            }}
           >
-            <FaCrown color={"#2b6cb0"} size={18} />
-            Upgrade to Pro{" "}
+            {isConnected ? <>{truncateText(address, 10)}</> : "Connect wallet"}
           </Button>
-          <Box
-            cursor="pointer"
-            p="8px"
-            rounded="full"
-            borderWidth="1px"
-            borderColor={getColorModeStyle("blackAlpha.200", "whiteAlpha.200")}
-            onClick={toggleColorMode}
-          >
-            {getColorModeStyle(<BsMoonFill />, <BsSunFill />)}
-          </Box>
+          <Flex align="center" gap="12px">
+            <Button
+              align="center"
+              gap="12px"
+              rounded="full"
+              variant="outline"
+              color={getColorModeStyle("blackAlpha.800", "whiteAlpha.800")}
+              borderColor={getColorModeStyle(
+                "blackAlpha.200",
+                "whiteAlpha.200"
+              )}
+              _hover={{
+                bg: getColorModeStyle("blackAlpha.50", "whiteAlpha.50"),
+              }}
+              fontSize={12}
+              onClick={onOpen}
+            >
+              <FaCrown color={"#2b6cb0"} size={18} />
+              Upgrade to Pro{" "}
+            </Button>
+            <Box
+              cursor="pointer"
+              p="8px"
+              rounded="full"
+              borderWidth="1px"
+              borderColor={getColorModeStyle(
+                "blackAlpha.200",
+                "whiteAlpha.200"
+              )}
+              onClick={toggleColorMode}
+            >
+              {getColorModeStyle(<BsMoonFill />, <BsSunFill />)}
+            </Box>
+          </Flex>
         </Flex>
-      </Flex>
-    </VStack>
+      </VStack>
+      {isOpen && <ProModal isOpen={isOpen} onClose={onClose} />}
+    </>
   );
 }
---
+++ src/components/ProModal.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  VStack,
+  Heading,
+  Box,
+  Text,
+  UnorderedList,
+  ListItem,
+  Flex,
+  Divider,
+  useColorMode,
+} from "@chakra-ui/react";
+import { BiCheck } from "react-icons/bi";
+import { BsXLg } from "react-icons/bs";
+
+export default function ProModal({ isOpen, onClose }) {
+  const { colorMode } = useColorMode();
+
+  const getColorModeStyle = (light, dark) =>
+    colorMode === "light" ? light : dark;
+
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent
+          maxW="380px"
+          py="24px"
+          px="12px"
+          bg={getColorModeStyle("white", "#17171D")}
+        >
+          {/* <ModalCloseButton /> */}
+          <ModalBody>
+            <VStack w="full" align="left" gap="12px">
+              <Box
+                w="fit-content"
+                px="18px"
+                py="8px"
+                fontWeight={700}
+                letterSpacing={1}
+                rounded="xl"
+                bg="blue.600"
+                color={getColorModeStyle("whiteAlpha.800", "whiteAlpha.800")}
+              >
+                PRO
+              </Box>
+              <Text
+                fontSize="14px"
+                color={getColorModeStyle("blackAlpha.600", "whiteAlpha.600")}
+              >
+                Get Our Advanced Features with Several Premium plan
+              </Text>
+              <Text
+                mt="18px"
+                color={getColorModeStyle("blue.600", "white")}
+                fontSize="20px"
+                fontWeight={600}
+              >
+                $0 for 3 Prompts, <br />
+                $2.99 per Month
+              </Text>
+              <Divider w="full" />
+              <UnorderedList
+                listStyleType="none"
+                fontSize={14}
+                spacing="10px"
+                w="full"
+                m="0"
+              >
+                <ListItem>
+                  {" "}
+                  <Flex align="center" gap="10px">
+                    <BiCheck size={20} /> Priority access to new features
+                  </Flex>
+                </ListItem>
+                <ListItem>
+                  <Flex align="center" gap="10px">
+                    <BiCheck size={20} /> Experience a 1 million token context
+                    window
+                  </Flex>
+                </ListItem>
+                <ListItem>
+                  <Flex align="center" gap="10px">
+                    <BiCheck size={20} /> 2 TB of storage
+                  </Flex>
+                </ListItem>
+                <ListItem>
+                  <Flex align="center" gap="10px">
+                    <BiCheck size={20} /> Unlimited Prompts
+                  </Flex>
+                </ListItem>
+              </UnorderedList>
+              <Divider w="full" />
+              <Flex w="full" align="center" justify="center" gap="16px">
+                <Button
+                  rounded="lg"
+                  size="md"
+                  color="white"
+                  bgGradient="linear(to-br, blue.600, blackAlpha.800)"
+                  w="full"
+                  alignSelf="flex-end"
+                  _hover={{
+                    bgGradient: "linear(to-tl, blue.600, blackAlpha.800)",
+                  }}
+                >
+                  Upgrade
+                </Button>
+                <Button variant="outline" onClick={onClose}>
+                  <BsXLg size={20} />
+                </Button>
+              </Flex>
+            </VStack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
